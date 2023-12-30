b69ea3e1e035a07d259a66bd07dbae58ac6c9464
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-30 19:30:26 +0530

    done updates:

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL="https://lmsbackend-api.onrender.com/api/v1"
+const BASE_URL="https://lmsbackend-api.onrender.com/api/v1/"
 const axiosInstance=axios.create()
 
 
